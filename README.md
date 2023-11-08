aa65b82955d53efa0378238fd5238559b5da8e87
Alejandro Ruiz <alejandroruizprieto@alumnos.tuniversformacion.es>
2023-11-08 10:02:06 +0100

    En proceso

---
+++ README.md
@@ -0,0 +1,3 @@
+# Proyecto Tema 3 Práctica Evaluable
+
+**Autor** : Alejandro Ruiz Prieto
---
+++ scripts/main.js
@@ -0,0 +1,73 @@
+/*
+        Alejandro Ruiz Prieto
+        Git-Hub:
+*/
+
+document.addEventListener("DOMContentLoaded", function () {
+  const nombre = prompt("Introduzca su nombre");
+  const apellido = prompt("Introduzca sus Apellidos");
+  const fechaNac = prompt("Introduzca su fecha de nacimiento DD/MM/YYYY");
+
+  const solicitarNombre = () => {
+    const nombre = prompt("Introduzca su nombre");
+    if (nombre === null || nombre.trim() === "") {
+      alert("Debe ingresar obligatoriamente su nombre");
+      solicitarNombre();
+    } else {
+      saludar(nombre);
+    }
+  };
+
+  const saludar = (nombre) => {
+    const saludo = document.createElement("div");
+    saludo.innerText = `¡Buenos días, ${nombre}!`;
+    document.body.appendChild(saludo);
+  };
+
+  solicitarNombre();
+
+  const nombreCompleto = () => {
+    const completo = document.createElement("div");
+    const nombreCompleto = nombre + " " + apellido;
+    const totalCaracteres = nombreCompleto.length;
+    completo.innerText = `Tu nombre completo (nombre y apellidos) tiene ${totalCaracteres} caracteres, incluyendo espacios.`;
+    document.body.appendChild(completo);
+  };
+  
+  nombreCompleto();
+
+
+
+
+
+
+  const primeraLetras = () => {};
+
+  const ultimaLetras = () => {};
+
+  const tresLetrasMenos = () => {};
+
+  const nombreMayus = () => {};
+
+  const mostrarEdad = () => {};
+
+  const estaciones = () => {};
+
+  const coseno = () => {};
+
+  const numeroMayores = () => {};
+
+  const numeroRandom = () => {};
+
+  /* const abrirNuevaVentana = () => {
+    window.open("https://www.tuniversformacion.es", "_blank");
+  };
+
+  document.getElementById("anchoVentana").textContent = window.innerWidth;
+
+  document.getElementById("urlActual").textContent = window.location.href;
+
+  const ventana = window.open('https://www.tuniversformacion.es' , 'width=800', 'height=600', 'resizable=no');
+
+  */
+});
